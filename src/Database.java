a9e083ceeab12d0f66010ce10f7117d255318469
RMizelle <64329293+drgazelle@users.noreply.github.com>
2023-01-28 17:00:35 -0500

    Rebased project to work with location objects

--- src/Database.java
+++ src/Database.java
@@ -1,17 +1,9 @@
-import com.google.gson.*;
-import com.google.gson.reflect.TypeToken;
-import kong.unirest.*;
-
 import javax.swing.JPanel;
-import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 
-import java.util.Collection;
 import java.util.Scanner;
 
 /** Database Class imports and exports
@@ -22,14 +14,14 @@ import java.util.Scanner;
  * @author RMizelle
  */
 public class Database extends JPanel {
-    private final ArrayList<String[]> database;
+    private final ArrayList<Location> locations;
     private File data;
 
     /** 0-arg constructor implements ArrayList of Countries
      *  of Cells objects from a text document.
      */
     public Database() {
-        database = new ArrayList<>();
+        locations = new ArrayList<>();
 
         //creates resource folder if necessary
         File directory = new File("resources");
@@ -72,32 +64,9 @@ public class Database extends JPanel {
             while (input.hasNextLine()) {
                 String line = input.nextLine();
                 //splits up data fields
-                String[] parts = line.split("##");
+                String[] parts = line.split("#");
                 //removes brackets from data fields
-                for(int i = 0; i < parts.length; i++) {
-                    parts[i] = parts[i].substring(1, parts[i].length() - 1).trim();
-                }
-                //gets name
-                String name = parts[0];
 
-                //get size
-                String[] temp = parts[1].split("x");
-                int[] size = new int[2];
-                size[0] = Integer.parseInt(temp[0].trim());
-                size[1] = Integer.parseInt(temp[1].trim());
-
-                //get live cells
-                ArrayList<int[]> cells = new ArrayList<>();
-                for(int i = 2; i < parts.length; i++) {
-                    //new splits String into parts and converts to ints
-                    int[] cell = new int[2];
-                    temp = parts[i].split(",");
-                    cell[0] = Integer.parseInt(temp[0].trim());
-                    cell[1] = Integer.parseInt(temp[1].trim());
-                    cells.add(cell);
-                }
-                //adds new MatrixData to database
-                //database.add(new MatrixData(name, size, cells));
             }
             //closes scanner
             input.close();
@@ -119,38 +88,7 @@ public class Database extends JPanel {
     }
 
     public int databaseSize() {
-        return database.size();
-    }
-
-    /** Accesses wikicollections api for patterns based of search term
-     *
-     * <p> https://rapidapi.com/timjacksonm-1jw8F2hFW3d/api/the-game-of-life </p>
-     *
-     * @param s search term
-     */
-    public void addFromSearch(String s) {
-
-        int count = 5;
-
-        HttpResponse<String> response = Unirest.get("https://the-game-of-life.p.rapidapi.com/wikicollection/search/title?value=" + s + "&select=%5B%22author%22%2C%22title%22%2C%22description%22%2C%22size%22%2C%22rleString%22%2C%22date%22%5D&count=" + count)
-                .header("X-RapidAPI-Key", "4ce993ab37mshadac634a5fbad3ep1a4c4fjsn041896a40067")
-                .header("X-RapidAPI-Host", "the-game-of-life.p.rapidapi.com")
-                .asString();
-
-        //error checks connection
-        if (response.getStatus() != 200) {
-            System.out.println("ERROR: Failed to Access WikiCollections API [Code" + response.getStatus() + "]");
-            Unirest.shutDown();
-            return;
-        }
-        System.out.println("Successful Connection [Code " + response.getStatus() + "]");
-        Unirest.shutDown();
-        Gson gson = new Gson();
-        JsonArray elements = JsonParser.parseString(response.getBody()).getAsJsonArray();
-
-        for (JsonElement e : elements) {
-            //results.add(gson.fromJson(e.getAsJsonObject(), MatrixData.class)); //ARCHIVED UNTIL FUNCTIONAL
-        }
+        return locations.size();
     }
 }
 
